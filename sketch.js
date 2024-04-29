3766b32ba085aa58e0f0ec1da7ba7b6bc6c36bc1
cfchen_tku <cfchengit@users.noreply.github.com>
2024-04-29 09:49:47 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,13 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
   capture = createCapture(VIDEO)  //啟動攝影機
-  capture.size(320,240);//設定顯示畫面大小
+  capture.size(640,480);//設定顯示畫面大小
 }
 
 function draw() {
   background(220);
   push()
-    translate(width/2-160,height/2-120) //把原點移到(width/2-160,height/2-120)
+    translate(width/2-320,height/2-240) //把原點移到(width/2-160,height/2-120)
     image(capture,0,0)  //在(0, 0)
   pop()
 }
